6ae0c07aeb6f311152655a8be41566375c5f8621
fmoorenoo <fermontemore@gmail.com>
2024-10-23 12:57:27 +0100

    Funcionalidad de buscar por titulos hecha

--- interfaz.py
+++ interfaz.py
@@ -10,12 +10,17 @@ from models.api_response import APIResponse
 
 def cargarProducto(lista_productos: APIResponse, direccion: int):
     global label_img, labelTitulo, labelDesc, labelPrecio, labelStock, labelRating, indice
+    if direccion == "anterior":
+        indice -= 1
+        if indice < 0:
+            indice = len(lista_productos.products) - 1
+    elif direccion == "siguiente":
+        indice += 1
+        if indice >= len(lista_productos.products):
+            indice = 0
+    else:
+        indice = direccion
 
-    indice += direccion
-    if indice >= len(lista_productos.products):
-        indice = 0
-    elif indice < 0:
-        indice = len(lista_productos.products) - 1
     r = requests.get(lista_productos.products[indice].thumbnail, stream=True)
     newImage = ImageTk.PhotoImage(Image.open(r.raw).resize((150, 150)))
     label_img.config(image=newImage)
@@ -30,11 +35,11 @@ def cargarProducto(lista_productos: APIResponse, direccion: int):
 
 
 def mostrarProducto(lista_productos: APIResponse):
-    global label_img, labelTitulo, labelDesc, labelPrecio, labelStock, labelRating, indice
+    global label_img, labelTitulo, labelDesc, labelPrecio, labelStock, labelRating, indice, boton_buscar
     indice = 0
     v_producto = tk.Tk()
     v_producto.title("Productos")
-    v_producto.geometry('745x265')
+    v_producto.geometry('775x265')
     v_producto.configure(bg='#B0C4C9')
     fuente = Font(family="Helvetica", size=16, weight="bold")
     frame = tk.Frame(v_producto)
@@ -50,7 +55,7 @@ def mostrarProducto(lista_productos: APIResponse):
     labelTitulo = ttk.Label(frame, text=lista_productos.products[indice].title, font=fuente)
     labelTitulo.grid(row=0, column=1, padx=10)
 
-    boton_buscar = ttk.Button(frame, text="Buscador", command=lambda: buscarProductos())
+    boton_buscar = ttk.Button(frame, text="Buscador", command=lambda: ventanaBuscador(lista_productos))
     boton_buscar.grid(row=0, column=2, padx=10, pady=20)
 
     labelDesc = ttk.Label(frame, text=lista_productos.products[indice].description, wraplength=300, anchor="w", justify="left")
@@ -65,20 +70,21 @@ def mostrarProducto(lista_productos: APIResponse):
     labelRating = ttk.Label(frame, text=f"Rating: {lista_productos.products[indice].rating}", font=fuente)
     labelRating.grid(row=2, column=2, padx=10)
 
-    boton_anterior = ttk.Button(frame, text="Anterior", command=lambda: cargarProducto(lista_productos, -1))
+    boton_anterior = ttk.Button(frame, text="Anterior", command=lambda: cargarProducto(lista_productos, "anterior"))
     boton_anterior.grid(row=3, column=0, padx=10, pady=20)
 
-    boton_siguiente = ttk.Button(frame, text="Siguiente", command=lambda: cargarProducto(lista_productos, 1))
+    boton_siguiente = ttk.Button(frame, text="Siguiente", command=lambda: cargarProducto(lista_productos, "siguiente"))
     boton_siguiente.grid(row=3, column=2, padx=10, pady=20)
 
 
     v_producto.mainloop()
 
-def buscarProductos():
-    global lista_productos
+def ventanaBuscador(lista_productos):
+    global boton_buscar
+    boton_buscar.config(state="disabled")
     v_busqueda = tk.Tk()
     v_busqueda.title("Buscador")
-    v_busqueda.geometry('745x265')
+    v_busqueda.geometry('655x70+630+300')
     v_busqueda.configure(bg='#B0C4C9')
     fuente = Font(family="Helvetica", size=16, weight="bold")
     frame = tk.Frame(v_busqueda)
@@ -89,5 +95,23 @@ def buscarProductos():
     entrada = ttk.Entry(frame, font=fuente)
     entrada.grid(row=0, column=1, padx=10)
 
-    boton_buscar = ttk.Button(frame, text="Buscar", command=False)
+
+    boton_buscar = ttk.Button(frame, text="Buscar", command=lambda : buscarProductos(entrada.get().lower(), lista_productos))
     boton_buscar.grid(row=0, column=2, padx=10, pady=20)
+
+    boton_cerrar = ttk.Button(frame, text="Cerrar ventana", command=lambda : cerrarBuscador(v_busqueda))
+    boton_cerrar.grid(row=0, column=3, padx=10, pady=0)
+
+
+def buscarProductos(titulo, lista_productos):
+    posicionProducto = 0
+    for x in range (len(lista_productos.products)):
+        if titulo in lista_productos.products[x].title.lower():
+            cargarProducto(lista_productos, posicionProducto)
+            break
+        posicionProducto += 1
+
+def cerrarBuscador(ventana):
+    global boton_buscar
+    boton_buscar.config(state="normal")
+    ventana.destroy()
